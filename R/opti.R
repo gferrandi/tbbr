5a4247c21619e96146d4f377f72e4b329366d10d
gferrandi <g.ferrandi@tue.nl>
2022-02-10 18:24:59 +0100

    first functions from the paper

---
+++ R/opti.R
@@ -0,0 +1,214 @@
+# norm2 --------
+#' L2 norm of a vector
+#'
+#' @param x vector
+#'
+#' @return L2 norm of a vector
+#'
+#' @examples
+#' norm2(c(1,2,3))
+norm2 <- function(x) {sqrt(sum(x^2))}
+
+# angle -------------------------------------------------------------------
+#' Angle between two vectors
+#'
+#' @param a first vector
+#' @param b second vector
+#'
+#' @return angle between the two vectors in radiants
+#'
+#' @examples
+#' angle(c(1,0,0), c(0,1,1))
+angle <- function(a,b){
+  a <- a/norm2(a); b <- b/norm2(b)
+  asin(min(1, norm2(a- sum(a*b)*b)))
+}
+
+#' Gallery of targets
+#'
+#' @param target_option string corresponding to one of the available targets (add which)
+#'
+#' @details e.g.,
+#' \tabular{ll}{
+#' Target \tab Description\cr 
+#' cotan1_1 \tab cotangent step with $q=r=1$ \cr
+#' }
+#'
+#' @return function with three arguments: angle between $s_k$ and $y_k$, inverse BB2 $\|y_k\|^2/s_k^Ty_k$, number of iterations
+#' @export
+#'
+#' @examples
+#' target_gallery("cotan1_1")
+target_gallery <- function(target_option){
+  
+  implemented <- c("cotan1_1", "cotan2_1", "cotan1_2", "cotan1_H", "cotanH_1", 
+                   "inv_bb2_2_01", "inv_bb2_100", "iter", "bb1", "bb2", "abb")
+  
+  if(!(target_option %in% implemented)) {
+    stop(cat("Target option not implemented. Try\n", paste0(implemented, collapse = "\n")))
+  }
+  
+  switch(target_option,
+         "cotan1_1"      = function (a,b,k) -cos(a)/sin(a),
+         "cotan2_1"      = function (a,b,k) -(cos(a))^2/sin(a),
+         "cotan1_2"      = function (a,b,k) -cos(a)/(sin(a))^2,
+         "cotan1_H"      = function (a,b,k) -cos(a)/sqrt(sin(a)),
+         "cotanH_1"      = function (a,b,k) -sqrt(cos(a))/sin(a),
+         "inv_bb2_2_01"  = function (a,b,k) 2.01*b,
+         "inv_bb2_100"   = function (a,b,k) 100*b,
+         "iter"          = function (a,b,k) {if(k == 0) 0 else (k+1)*b})
+}
+
+# parameters of sg --------------------------------------------------------
+#' Parameters of global TBB
+#'
+#' @param alpha_min min value for the stepsize, default = 1e-30
+#' @param alpha_max max value for the stepsize, default = 1e30
+#' @param alpha initial value for the stepsize, default = 1
+#' @param gamma line search param, default = 1e-4
+#' @param M number of stored previous function evaluations, default = 10
+#' @param tol tolerance for the stopping criterion, default = 1e-6
+#' @param iter_max maximum number of iterations, default = 1e4
+#' @param iter_int_max maximum number of iterations for the backtracking, default = 100
+#' @param target_option string or function. See target_gallery for more details; default = "bb1"
+#' @param ... additional arguments (not needed at the moment)
+#'
+#' @return list with parameters for the 
+#' @export
+#'
+#' @examples
+#' gtbb_control()
+gtbb_control <- function(alpha_min = 1e-8, alpha_max = 1e30, alpha = NULL, 
+                               gamma = 1e-4, M = 5, tol = 1e-6, iter_max = 1e4, iter_int_max = 30, 
+                               target_option = "bb1", ...){
+  args <- list(...)
+  c(list(alpha_min = alpha_min, alpha_max = alpha_max, alpha = alpha, 
+         gamma = gamma, M = M, tol = tol, iter_max = iter_max, iter_int_max = iter_int_max, 
+         target_option = target_option), args)
+}
+
+# unconstrained spectral gradient + nonmonotone ls ------------------------
+#' Global TBB 
+#' 
+#' Gradient method with nonmonotone line search for continuously differentiable functions. 
+#' 
+#' It is endowed with the TBB stepsize, a Barzilai--Borwein like stepsize with an additional parameter, called target.
+#'
+#' @param x starting vector
+#' @param obj objective function. Must be of the form obj(x)
+#' @param grad gradient function. Must be of the form grad(x)
+#' @param p control parameters. See gtbb_control for more details
+#' @param quadratic whether the objective function is quadratic or not, default = FALSE
+#' @param reltol whether the tolerance is multiplied by the norm of the first gradient (TRUE) or not. default = TRUE
+#' @param info save information of the whole method, default = FALSE
+#'
+#' @return minimizer and some information: number of function evaluations, number of iterations, last gradient norm, tolerance, final objective value.
+#' It optionally returns the aforementioned information about all the iterations.
+#' @export
+#'
+#' @examples
+#' A <- c(1:100)
+#' x <- rep(1:100)
+#' 
+#' obj <- function(x) sum(A * x^2))/2
+#' grad <- function(x) A * x
+#' 
+#' generic_sg(x, obj, grad, quadratic = TRUE)
+gtbb <- function(x, obj, grad, p = generic_sg_control(), quadratic = FALSE, reltol = TRUE, info = FALSE){
+  
+  # get params
+  p <- do.call(gtbb_control, as.list(p))
+  
+  # define target function
+  if(!is.function(p$target_option)){
+    tau_fun <- target_gallery(p$target_option)
+  }
+  
+  # init f, gradient, alpha
+  f <- obj(x); g <- grad(x)
+  alpha <- if(is.null(p$alpha)) max(p$alpha_min, min(1/norm2(g), p$alpha_max)) else p$alpha
+  alpha0 <- alpha
+  
+  # init objects to store previous iterations
+  if(!quadratic){f_hist <- rep(f, p$M)}
+  
+  # save for reproducibility
+  x0 <- x
+  
+  # tracking parameters
+  if(info) alg_log <- NULL; 
+  nfe <- 1; const_step <- 0
+  
+  # relative tolerance
+  if(reltol) p$tol <- p$tol*norm2(g)
+  
+  tryCatch({
+    for(iter in 0:(p$iter_max-1)){
+      
+      # stopping criterion
+      sng <- sum(g^2)
+      ng <- sqrt(sng)
+      
+      # save info 
+      if(info){
+        iter_info <- c(iter = iter, stop_criterion = ng, obj = f, alpha = alpha, nfe = nfe)
+        alg_log <- rbind(alg_log, iter_info)
+      }
+      
+      # is current point stationary? 
+      if(ng < p$tol) break
+      
+      # START BACKTRACKING 
+      if(!quadratic){
+        for (k in 1:p$iter_int_max) {
+          xnew <- x - alpha*g; f <- obj(xnew); nfe <- nfe + 1
+          if (f <= max(f_hist, na.rm = TRUE)-p$gamma*alpha*sng) break else alpha <- alpha/2
+        }
+        x <- xnew
+        f_hist<- if(p$M == 1) f else c(f_hist[2:p$M], f) # save previous function value
+        # END BACKTRACKING 
+      } else {
+        x <- x -alpha*g; f <- obj(x); nfe <- nfe + 1
+      }
+      
+      # update gradients
+      g0 <- g; g <- grad(x); y <- g - g0
+      
+      # update stepsize 
+      p0y <- sum(g0*y); yy <- sum(y*y); sy <- -alpha*p0y
+      
+      if(p$target_option == "bb1"){
+        alpha <- -alpha*sng/p0y
+      } else if(p$target_option == "bb2"){
+        alpha <- sy/yy
+      } else if(p$target_option == "abb"){
+        if(cos(angle(g0, y))^2 < 0.8) alpha <- sy/yy else alpha <- -alpha*sng/p0y
+      } else {
+        tauk <- tau_fun(angle(g0, y),yy/sy,iter)
+        alpha <- (sy-tauk*alpha^2*sng)/(yy-tauk*sy)  
+      }
+      
+      if(!quadratic){
+        if(alpha > 0) {alpha0 <- alpha} else {alpha <- alpha0; const_step <- const_step + 1}
+        alpha <- max(p$alpha_min, min(alpha, p$alpha_max)) 
+      }
+    }
+  },
+  error = function(e){
+    cat("ERROR:", conditionMessage(e), "\n")
+    return(NA)
+  }
+  )
+  
+  # prepare output
+  last_iter <- data.frame(id_algo = p$target_option, iter = iter, nfe = nfe, stop_criterion = ng, obj = f)
+  last_iter$const_step <- const_step
+  last_iter$tol <- p$tol
+  
+  if(info){
+    alg_log <- as.data.frame(alg_log); rownames(alg_log) <- NULL
+    return(list(alg_log = alg_log, last_iter = last_iter, x = x))
+  } else {
+    return(list(last_iter = last_iter, x = x))
+  }
+}
---
+++ R/unconstrained_gallery.R
@@ -0,0 +1,131 @@
+#' Unconstrained problems
+#'
+#' Gallery of unconstrained optimization problems from different sources. They are all based on sum of functions, where the number of terms is related to the size of the problem.
+#'
+#' @param exp string that identifies the experiment in the gallery
+#' @param n dimension of the problem 
+#'
+#' @details e.g.,
+#' \tabular{lll}{
+#' Name \tab Function \tab Reference \cr
+#' 2 \tab Strictly convex 2 \tab Raydan 1997\cr
+#' }
+#' 
+#' @md
+#'
+#' @return a list with suggested starting point, objective and gradient functions
+#' @export
+#'
+#' @examples
+#' function_gallery("genWH", 10)
+function_gallery <- function(exp, n){
+  define_f <- switch(exp,
+                     "3" = function(x, n){
+                       f          <- rep(0, n)
+                       f[1:(n-1)] <- x[1:(n-1)] - (n+1) + sum(x)
+                       f[n]       <- prod(x) - 1
+                       f
+                     },
+                     "4" = function(x, n){
+                       n - sum(cos(x)) + c(1:n)*(1-cos(x)) - sin(x)
+                     },
+                     "5" = function(x, n){
+                       (3-2*x)*x - c(0, x[1:(n-1)]) - 2*c(x[2:n], 0) + 1
+                     },
+                     "7" = function(x, n){ # extended rosenbrock (easy version)
+                       odd <- as.logical(1:n %% 2)
+                       f <- rep(0, n)
+                       f[odd] <- 10*(x[!odd] - x[odd]^2)
+                       f[!odd] <- 1 - x[odd]
+                       f
+                     },
+                     "8" = function(x, n){
+                       f <- sqrt(1e-5)*(x - 1)
+                       c(f, sum(x^2) - 1/4)
+                     },
+                     "10" = function(x, n){
+                       f      <- rep(0, n)
+                       f[1:n] <- x[1:n]-1
+                       f[n+1] <- sum(c(1:n)*f[1:n])
+                       f[n+2] <- f[n+1]^2
+                       f
+                     },
+                     "11" = function(x, n){
+                       f <- rep(0,n); i <- 1:n %% 4
+                       f[i == 0] <- sqrt(10)*(x[i == 1] - x[i == 0])^2
+                       f[i == 3] <- (x[i == 2] - 2*x[i ==3])^2
+                       f[i == 2] <- sqrt(5)*(x[i == 3] - x[i == 0])
+                       f[i == 1] <- x[i == 1] + 10*x[i == 2]
+                       f
+                     }
+  )
+  
+  exp_ls <- switch(exp,
+                   "1" = list(x0    = c(1:n)/n,
+                              obj   = function(x) {sum(exp(x) - x)},
+                              grad  = function(x) {exp(x) - 1}),
+                   "2" = list(x0    = rep(1,n),
+                              obj   = function(x) {sum(c(1:n)*(exp(x) - x))/10},
+                              grad  = function(x) {c(1:n)*(exp(x) - 1)/10}),
+                   "3" = list(x0    = rep(0.5, n),
+                              obj   = function(x) {sum(define_f(x, n)^2)/2},
+                              grad  = function(x) {f <- define_f(x, n)
+                              out <- c(f[1:(n-1)],0) + sum(f[1:(n-1)])
+                              tmp <- numeric(); for(i in 1:n) {tmp[i] <- prod(x[-i])}
+                              out + f[n]*tmp
+                              }),
+                   "4" = list(x0    = rep(1,n)/n,
+                              obj   = function(x) {sum(define_f(x, n)^2)/2},
+                              grad  = function(x) {f <- define_f(x, n); sum(f)*sin(x) + f*(c(1:n)*sin(x) - cos(x))}),
+                   "5" = list(x0    = rep(-1,n),
+                              obj   = function(x) {sum(define_f(x, n)^2)/2},
+                              grad  = function(x) {f <- define_f(x, n); f*(3-4*x) - c(f[2:n], 0) - 2*c(0, f[1:(n-1)])}),
+                   "7" = list(x0    = rep(c(-1.2, 1), floor(n/2)),
+                              obj   = function(x) {sum(define_f(x, n)^2)/2},
+                              grad  = function(x) {f <- define_f(x, n); odd <- as.logical(1:n %% 2); g <- rep(0,n)
+                              g[odd] <- -20*f[odd]*x[odd] - f[!odd]; g[!odd] <- 10*f[odd]; g
+                              }),
+                   "8" = list(x0    = c(1:n),
+                              obj   = function(x) {sum(define_f(x, n)^2)/2},
+                              grad  = function(x) {f <- define_f(x, n); sqrt(1e-5)*f[1:n] + 2*f[n+1]*x}),
+                   "10" = list(x0   = 1 - c(1:n)/n,
+                               obj  = function(x) {sum(define_f(x, n)^2)/2},
+                               grad = function(x) {f <- define_f(x, n); f[1:n] + c(1:n)*f[n+1]*(1 + 2*f[n+2])}),
+                   "11" = list(x0   = rep(c(3,-1,0,1), floor(n/4)),
+                               obj  = function(x) {sum(define_f(x, n)^2)/2},
+                               grad = function(x) {f <- define_f(x, n); i <- 1:n %% 4; g <- rep(0,n)
+                               g[i == 0] <- -2*sqrt(10)*f[i == 0]*(x[i == 1] - x[i == 0]) - sqrt(5)*f[i == 2]
+                               g[i == 3] <- -4*f[i == 3]*(x[i == 2] - 2*x[i == 3]) + sqrt(5)*f[i == 2]
+                               g[i == 2] <- 2*f[i == 3]*(x[i == 2] - 2*x[i == 3]) + 10*f[i == 1]
+                               g[i == 1] <- 2*sqrt(10)*f[i == 0]*(x[i == 1] - x[i == 0]) + f[i == 1]
+                               g
+                               }),
+                   "12" = list(x0    = rep(c(-1.2, 1), floor(n/2)), # n must be even (?)
+                               obj   = function(x) {sum(100*(x[2:n] - x[1:(n-1)]^2)^2 + (1-x[1:(n-1)])^2)/2},
+                               grad  = function(x) {g <- c(x[1:(n-1)]-1,0) 
+                               g[1] <- g[1] -200*(x[2] - x[1]^2)*x[1]
+                               g[2:(n-1)] <- g[2:(n-1)] - 200*(x[3:n] - x[2:(n-1)]^2)*x[2:(n-1)] +100*(x[2:(n-1)] - x[1:(n-2)]^2)
+                               g[n] <- g[n] + 100*(x[n] - x[n-1]^2)
+                               g
+                               }),
+                   "diag5" = list(x0 = rep(1,n), # too easy
+                                  obj = function(x){sum(log(exp(x)+exp(-x)))},
+                                  grad = function(x){(exp(x)-exp(-x))/(exp(x)+exp(-x))}),
+                   "hager" = list(x0 = rep(1,n),
+                                  obj = function(x){sum(exp(x) - sqrt(c(1:n))*x)},
+                                  grad = function(x){exp(x) - sqrt(c(1:n))}),
+                   "gentridiag1" = list(x0 = rep(2, n),
+                                        obj = function(x){sum((x[2:n] + x[1:(n-1)] - 3)^2 + (x[1:(n-1)] -x[2:n] + 1)^4)/2},
+                                        grad = function(x){c(x[2:n] + x[1:(n-1)] - 3, 0) + c(0, x[2:n] + x[1:(n-1)] - 3) +
+                                            2*c(-x[2:n] + x[1:(n-1)] + 1, 0)^3 - 2*c(0, -x[2:n] + x[1:(n-1)] + 1)^3}),
+                   "genWH" = list(x0    = rep(c(-1.2, 1), floor(n/2)), # n must be even (?)
+                                  obj   = function(x) {sum(100*(x[2:n] - x[1:(n-1)]^3)^2 + (1-x[1:(n-1)])^2)/2},
+                                  grad  = function(x) {g <- c(x[1:(n-1)]-1,0) 
+                                  g[1] <- g[1] -300*(x[2] - x[1]^3)*(x[1]^2)
+                                  g[2:(n-1)] <- g[2:(n-1)] - 300*(x[3:n] - x[2:(n-1)]^3)*(x[2:(n-1)]^2) +100*(x[2:(n-1)] - x[1:(n-2)]^3)
+                                  g[n] <- g[n] + 100*(x[n] - x[n-1]^3)
+                                  g
+                                  })
+  )
+  exp_ls
+}
